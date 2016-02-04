ade5b06f7a3c4b2314699c6b503a42aee21f5a1d
Sean Gofus <sean.gofus@advisorsexcel.com>
2016-02-03 20:43:12 -0600

    Added quick app stub.

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', function (req, res) {
+
+    var body = {
+        response_type: "in_channel",
+        text: ":badjokeeel: " + req.body.text
+    };
+
+    res.send(body);
+});
+
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!');
+});
