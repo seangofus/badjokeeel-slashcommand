b449163d592cf27adfcd65a825d37513633cd839
Sean Gofus <sean.gofus@advisorsexcel.com>
2016-02-06 11:44:22 -0600

    Requiring the rpi code.

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var ws281x = require('rpi-ws281x-native');
+
+var NUM_LEDS = parseInt(process.argv[2], 10) || 1,
+    pixelData = new Uint32Array(NUM_LEDS);
+
+ws281x.init(NUM_LEDS);
+
 app.post('/', function (req, res) {
+    console.log(NUM_LEDS);
+
     var user = req.body.user_name;
 
     var body = {
