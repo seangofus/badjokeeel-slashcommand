8500880f2796fdafef15945fdd62b2a8c09ec016
Sean Gofus <sean.gofus@advisorsexcel.com>
2016-02-07 08:33:02 -0600

    Added some test code to light up neo pixel on slash command.

--- app.js
+++ app.js
@@ -12,10 +12,32 @@ var NUM_LEDS = parseInt(process.argv[2], 10) || 1,
 
 ws281x.init(NUM_LEDS);
 
-app.post('/', function (req, res) {
-    console.log(NUM_LEDS);
+function startUpShutDown (process, colorString) {
+    if (null !== colorString) {
+        var colorArray = colorString.split(colorString);
+    }
 
+    pixelData[0] = color(0, 0, 0);
+    ws281x.render(pixelData);
+
+    if ('startup' === process) {
+        pixelData[0] = color(colorArray[0], colorArray[1], colorArray[2]);
+        ws281x.render(pixelData);
+    }
+
+    if ('shutdown' === process) {
+        pixelData[0] = color(0, 0, 0);
+        ws281x.render(pixelData);
+        ws281x.reset();
+    }
+}
+
+app.post('/', function (req, res) {
     var user = req.body.user_name;
+    var color = '0,153,255';
+    if ('sean' === user) {
+        var color = '255,0,255';
+    }
 
     var body = {
         response_type: "in_channel",
@@ -23,6 +45,10 @@ app.post('/', function (req, res) {
     };
 
     res.send(body);
+    startUpShutDown('startup', color);
+    setTimeout(function() {
+        startUpShutDown('shutdown', null);
+    }, 5000);
 });
 
 app.listen(8080, function () {
